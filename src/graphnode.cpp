ce7ba7993ccb1a2f2def1e90507b9093dac3a818
Nícholas André <nicholas.andre@10up.com>
2021-05-18 21:23:49 -0300

    Move ownership of chatbot

--- src/graphnode.cpp
+++ src/graphnode.cpp
@@ -32,7 +32,7 @@ void GraphNode::MoveChatbotHere(ChatBot chatbot)
 
 void GraphNode::MoveChatbotToNewNode(GraphNode *newNode)
 {
-    newNode->MoveChatbotHere(_chatBot);
+    newNode->MoveChatbotHere(std::move(_chatBot));
 }
 
 GraphEdge *GraphNode::GetChildEdgeAtIndex(int index)
